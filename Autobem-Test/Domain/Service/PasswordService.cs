dd0fae8325333fd8aac7f04c1da3685cf1bde459
thyerry <thyerry.nunes@gmail.com>
2023-06-25 11:42:42 -0300

    Add PasswordService

--- Autobem-Test/Domain/Service/PasswordService.cs
+++ Autobem-Test/Domain/Service/PasswordService.cs
@@ -1,21 +1,45 @@
 ﻿using Domain.Contracts.Service;
 using Microsoft.Extensions.Configuration;
+using System.Security.Cryptography;
+using System.Text;
 
-namespace Domain.Service
+namespace Domain.Service;
+
+public class PasswordService : IPasswordService
 {
-    public class PasswordService : IPasswordService
+    private readonly string? _pepper;
+
+    public PasswordService(IConfiguration configuration)
     {
-        private readonly string? _pepper;
+        _pepper = configuration.GetSection("PasswordPepper")["pepper"];
+    }
 
-        public PasswordService(IConfiguration configuration)
-        {
-            _pepper = configuration.GetSection("PasswordPepper")["pepper"];
-        }
+    public virtual string HashPassword(string password, string salt = "", int iteration = 3)
+    {
+        if (iteration <= 0)
+            return password;
 
-        public virtual string HashPassword(string password, string salt = "", int iteration = 3)
+        if (string.IsNullOrWhiteSpace(salt))
+            salt = GenerateSalt();
+
+        using (var sha256 = SHA256.Create())
         {
-            if(iteration <= 0) return password;
-            return HashPassword(password, salt, iteration - 1);
-        }
+            var passwordSaltPepper = $"{password}{salt}{_pepper}";
+            var byteValue = Encoding.UTF8.GetBytes(passwordSaltPepper);
+            var byteHash = sha256.ComputeHash(byteValue);
+            var hash = Convert.ToBase64String(byteHash);
+            return HashPassword(hash, salt, iteration - 1);
+        };
+    }
+
+    public virtual string GenerateSalt()
+    {
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            var byteSalt = new byte[16];
+            rng.GetBytes(byteSalt);
+            var salt = Convert.ToBase64String(byteSalt);
+            return salt;
+        };
     }
 }
--- Autobem-Test/Tests/UnitTests/Domain/Service/PasswordServiceTest.cs
+++ Autobem-Test/Tests/UnitTests/Domain/Service/PasswordServiceTest.cs
@@ -8,7 +8,9 @@ public class PasswordServiceTest
 {
     private const string PASSWORD = "password";
     private const string SALT = "salt";
-    private const int THREE_HASHES = 3;
+    private const int ZERO_ITERATIONS = 0;
+    private const int TWO_ITERATIONS = 2;
+    private const int THREE_ITERATIONS = 3;
     private const string CONFIGURATION_KEY = "Key";
 
     public class FakeConfiguration : IConfiguration
@@ -57,6 +59,8 @@ public class PasswordServiceTest
         }
     }
 
+    #region HashPassword
+
     [Fact]
     public void HashPassword_OnOnlyPassingPassword_ReturnString()
     {
@@ -68,6 +72,26 @@ public class PasswordServiceTest
 
         // Assert
         result.Should().BeOfType<string>();
+    }
+
+    [Fact]
+    public void HashPassword_OnOnlyPassingPassword_ReturnStringMustBeDifferentFromPassword()
+    {
+        // Arrange
+        var mockService = new Mock<PasswordService>(new FakeConfiguration());
+        mockService
+            .Setup(p => p.HashPassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()))
+            .CallBase();
+        mockService
+            .Setup(p => p.GenerateSalt())
+            .CallBase();
+        var sut = mockService.Object;
+
+        // Act
+        var result = sut.HashPassword(PASSWORD);
+
+        // Assert
+        result.Should().NotBeEquivalentTo(PASSWORD);
     }
 
     [Fact]
@@ -87,7 +111,116 @@ public class PasswordServiceTest
         // Assert
         mockService.Verify(service =>
             service.HashPassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()),
-            Times.Exactly(THREE_HASHES + 1)
+            Times.Exactly(THREE_ITERATIONS + 1)
         );
     }
+
+    [Fact]
+    public void HashPassword_OnPassingIterationCount_ShouldBeCalledIterationPlusOneTimes()
+    {
+        // Arrange
+        var mockService = new Mock<PasswordService>(new FakeConfiguration());
+        mockService
+            .Setup(p => p.HashPassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()))
+            .CallBase();
+
+        var sut = mockService.Object;
+
+        // Act
+        sut.HashPassword(PASSWORD, iteration: TWO_ITERATIONS);
+
+        // Assert
+        mockService.Verify(service =>
+            service.HashPassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()),
+            Times.Exactly(TWO_ITERATIONS + 1)
+        );
+    }
+
+    [Fact]
+    public void HashPassword_OnPassingIterationCountEqualsZero_ShouldReturnSamePassword()
+    {
+        // Arrange
+        var sut = new PasswordService(new FakeConfiguration());
+
+        // Act
+        var result = sut.HashPassword(PASSWORD, iteration: ZERO_ITERATIONS);
+
+        // Assert
+        result.Should().BeEquivalentTo(PASSWORD);
+    }
+
+    [Fact]
+    public void HashPassword_OnPassingNoSalt_ShouldCallGenerateSaltMethod()
+    {
+        // Arrange
+        var mockService = new Mock<PasswordService>(new FakeConfiguration());
+        mockService
+            .Setup(p => p.HashPassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()))
+            .CallBase();
+        mockService
+            .Setup(p => p.GenerateSalt())
+            .CallBase();
+
+        var sut = mockService.Object;
+
+        // Act
+        sut.HashPassword(PASSWORD);
+
+        // Assert
+        mockService.Verify(service => service.GenerateSalt(), Times.Once);
+    }
+
+    [Fact]
+    public void HashPassword_OnPassingSalt_ShouldNotCallGenerateSaltMethod()
+    {
+        // Arrange
+        var mockService = new Mock<PasswordService>(new FakeConfiguration());
+        mockService
+            .Setup(p => p.HashPassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()))
+            .CallBase();
+        mockService
+            .Setup(p => p.GenerateSalt())
+            .CallBase();
+
+        var sut = mockService.Object;
+
+        // Act
+        sut.HashPassword(PASSWORD, salt: SALT);
+
+        // Assert
+        mockService.Verify(service => service.GenerateSalt(), Times.Never);
+    }
+
+    #endregion HashPassword
+
+    #region GenerateSalt
+
+    [Fact]
+    public void GenerateSalt_ReturnString()
+    {
+        // Arrange
+        var sut = new PasswordService(new FakeConfiguration());
+
+        // Act
+        var result = sut.GenerateSalt();
+
+        // Assert
+        result.Should().BeOfType<string>();
+    }
+
+    [Fact]
+    public void GenerateSalt_CallingTwoTimes_ReturnDifferentString()
+    {
+        // Arrange
+        var sut = new PasswordService(new FakeConfiguration());
+
+        // Act
+        var resultOne = sut.GenerateSalt();
+        var resultTwo = sut.GenerateSalt();
+
+        // Assert
+        resultOne.Should().NotBeEquivalentTo(resultTwo);
+    }
+
+    #endregion GenerateSalt
 }
